054e6b57d75777f2c47bd665664b44f37a38664e
Rakotondrazanajatovo Sandratra Mathieu <szanajatovo@gmail.com>
2025-07-14 10:16:08 +0900

    change db

--- inc/connexion.php
+++ inc/connexion.php
@@ -4,8 +4,8 @@ function dbconnect()
     static $connect = null;
 
     if ($connect === null) {
-        //$connect = mysqli_connect('localhost', 'root', '', 'projet_s2');
-        $connect = mysqli_connect('172.60.0.15', 'ETU004051', '9oAEIdGq', 'db_s2_ETU004051');
+        $connect = mysqli_connect('localhost', 'root', '', 'projet_s2');
+        //$connect = mysqli_connect('172.60.0.15', 'ETU004051', '9oAEIdGq', 'db_s2_ETU004051');
 
         if (!$connect) {
             die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
