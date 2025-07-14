0f150737df0151c490ebd357fd69db760f5d4279
Rakotondrazanajatovo Sandratra Mathieu <szanajatovo@gmail.com>
2025-07-14 10:33:51 +0900

    add lines search

--- pages/accueil.php
+++ pages/accueil.php
@@ -29,6 +29,16 @@
         </select>
         <input type="submit" value="Filtrer">
     </form>
+    <h3>Search</h3>
+    <form action="traitement/traitement-recherche.php" method="get">
+        <select name="categorie" id="">
+            <?php foreach($categories as $categorie) { ?>
+                <option value="<?= $categorie['nom_categorie'] ?>"><?= $categorie['nom_categorie'] ?></option>
+            <?php } ?>
+        </select>
+        <input type="text" name="nom_objet">
+        <button>Search</button>
+    </form>
     <form action="../inc/deconnexion.php">
         <input type="submit" value="Déconnecter">
     </form>
@@ -37,19 +47,18 @@
         <div class="row">
         <?php foreach($objets as $objet) { ?>
             <div class="col-md-3">
-
-                    <div class="card mt-3">
-                        <img src="../assets/<?= $objet['nom_image'] ?>" class="card-img-top" alt="<?= $objet['nom_image'] ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?=  $objet['nom_objet'] ?></h5>
-                            <p class="card-text text-secondary"><?= $objet['nom'] ?></p>
-                            <hr>
-                            <h5 class="text-secondary"> <?= $objet['date_retour'] ?></h5>
-                        </div>
-                        <div class="position-absolute top-0 end-0 p-2">
-                            <button type="button" class="btn btn-danger"><?= $objet['nom_categorie']  ?></button>
-                        </div>
+                <div class="card mt-3">
+                    <img src="../assets/<?= $objet['nom_image'] ?>" class="card-img-top" alt="<?= $objet['nom_image'] ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?=  $objet['nom_objet'] ?></h5>
+                        <p class="card-text text-secondary"><?= $objet['nom'] ?></p>
+                        <hr>
+                        <h5 class="text-secondary"> <?= $objet['date_retour'] ?></h5>
                     </div>
+                    <div class="position-absolute top-0 end-0 p-2">
+                        <button type="button" class="btn btn-danger"><?= $objet['nom_categorie']  ?></button>
+                    </div>
+                </div>
             </div>
         <?php } ?>
     </div>
